afbabf94af6ce9ca3e37270ade28590b7a65ff52
paulSalazar <70818490+paulsalazar24@users.noreply.github.com>
2023-07-10 20:34:08 -0500

    planilla

---
+++ src/main/java/com/mycompany/planillavirtual/PlanillaVirtual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.planillavirtual;
+
+/**
+ *
+ * @author quisp
+ */
+public class PlanillaVirtual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
